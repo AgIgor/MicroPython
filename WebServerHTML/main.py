ed17287dc4751ec4b3cb2adcbfbbc5cb69298f77
AgIgor <agigor@outlook.com>
2023-02-01 21:47:43 -0300

    first commit

---
+++ WebServerHTML/main.py
@@ -0,0 +1,69 @@
+try:
+  import usocket as socket
+except:
+  import socket
+
+from machine import Pin
+import network
+
+import esp
+esp.osdebug(None)
+
+import gc
+gc.collect()
+
+ssid = 'VIVOFIBRA-9501'
+password = 'rgw7ucm3GT'
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(station.ifconfig())
+
+led = Pin(2, Pin.OUT)
+
+def web_page():
+  if led.value() == 1:
+    gpio_state="ON"
+  else:
+    gpio_state="OFF"
+  
+  with open("index.html") as f:
+      result = f.read()
+      page = result.replace("gpio_state",gpio_state)
+      return page
+      
+   
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 80))
+s.listen(5)
+
+while True:
+  conn, addr = s.accept()
+  print('Got a connection from %s' % str(addr))
+  request = conn.recv(1024)
+  request = str(request)
+  print('Content = %s' % request)
+  led_on = request.find('/?btn=on')
+  led_off = request.find('/?btn=off')
+  
+  if led_on == 6:
+    print('LED ON')
+    led.value(1)
+  if led_off == 6:
+    print('LED OFF')
+    led.value(0)
+    
+  response = web_page()
+  conn.send('HTTP/1.1 200 OK\n')
+  conn.send('Content-Type: text/html\n')
+  conn.send('Connection: close\n\n')
+  conn.sendall(response)
+  conn.close()
